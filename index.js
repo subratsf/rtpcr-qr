0bbc7fae5600ab2ef6623286537e0f7b74129497
subrat.thakur <subrat.thakur@salesforce.com>
2021-07-12 22:57:51 +0530

    added pdf changes

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const fs = require('fs')
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res) => {
+    var tempFile="./Rajesh_Thakur_RTPCR.pdf";
+    fs.readFile(tempFile, function (err,data){
+        res.contentType("application/pdf");
+        res.send(data);
+    });
+});
+
+app.listen(port, () => {
+  console.log(`RTPCR listening on port ${port}!`)
+});
