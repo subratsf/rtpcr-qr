4d4c2d58eeec6fb824c036bf16d2624f3283aff4
subrat.thakur <subrat.thakur@salesforce.com>
2021-07-12 23:09:20 +0530

    added dynamic port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req, res) => {
     var tempFile="./Rajesh_Thakur_RTPCR.pdf";
